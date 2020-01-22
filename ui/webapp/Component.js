c59870b4cd0ff517b04ebdc3aae0ed0d390e6f1e
Marius Obert <marius.obert@sap.com>
2020-01-22 18:19:29 +0100

    Remove unneeded files Put file that shouldn't be tracked with git on gitignore Update UI5 app to load async Rename view cut dead code

--- ui/webapp/Component.js
+++ ui/webapp/Component.js
@@ -1,30 +1,20 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"con/sap/ui/model/models"
-], function (UIComponent, Device, models) {
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
 	"use strict";
 
-	return UIComponent.extend("con.sap.ui.Component", {
+	return UIComponent.extend("demo.sap.msgraph.Component", {
 
 		metadata: {
 			manifest: "json"
 		},
 
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
 		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// enable routing
 			this.getRouter().initialize();
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
 		}
 	});
 });
